a5d68a053c85ede6c15a298320df42861ecb2acd
ptobechi <ptobechii@gmail.com>
2023-07-17 14:59:01 -0700

    lasr digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,9 @@ int main(void)
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);;
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	else
+	else if (last_digit < 6 !== 0)
 	{
 		printf("Last digit of %d is less than 6 and not zero\n", n);
 	}
