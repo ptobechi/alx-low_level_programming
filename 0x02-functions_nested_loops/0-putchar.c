4205d7d2fbb63228c00b9fd44f49f86f8512e68e
ptobechi <ptobechii@gmail.com>
2023-07-18 16:32:32 -0700

    pustchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Result: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * File: main.h
+ * Author: Tobechukwu Paschal
+ * Description: This contains the declaration of all functions used on 0x02-functions_nested_loops project for C Language
+*/
+int _putchar(char c);
+#endif
