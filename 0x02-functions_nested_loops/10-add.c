d247d2ecffd62a85ba7a2efbe7ccfcd18dd37da4
ptobechi <ptobechii@gmail.com>
2023-07-20 00:40:24 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two nnumbers and return results
+ * @a - first int 
+ * $b - second int 
+ * Return - Result of a and b addition
+ */
+
+int add(int a int b)
+{
+	int answer;
+	
+	answer = a +b;
+
+	return (answer);
+}
