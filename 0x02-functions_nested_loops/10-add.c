c14d2593d18379d03814ed666cd57d092ceda273
ptobechi <ptobechii@gmail.com>
2023-07-20 00:50:04 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - add two nnumbers and return results
- * @a: - first int 
- * @b:  - second int 
+ * @a: first int 
+ * @b: second int 
  * Return: - Result of a and b addition
 */
 int add(int a, int b)
