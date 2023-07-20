65c85df1d2e9fb6daea4063b86689cd61c3777e5
ptobechi <ptobechii@gmail.com>
2023-07-20 10:19:21 -0700

    positive or negativecheeck for zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negativ
+* main - Entry Point
 * Return: 0
 */
 int main(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	positive_or_negative(i);
 	return (0);
