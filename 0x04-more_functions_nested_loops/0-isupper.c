c3f8e240390ff80b5d1c30c7bc34798622be1182
ptobechi <ptobechii@gmail.com>
2023-07-20 04:47:10 -0700

    complete project for more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,9 +5,13 @@
  * _isupper - returns 1 if true else return 0
  * @c: check if uppercase.
  *
- * Return: Always 0.
+ * Return: Always 0 or 1
  */
 int _isupper(int c)
 {
-    return ((c >= 65) && (c <= 90));
+    if (c > 64 && c < 91)
+    {
+	    return (1)
+    }
+    return (0);
 }
