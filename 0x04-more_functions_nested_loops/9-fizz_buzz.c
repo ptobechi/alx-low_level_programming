b4f4a846b8851a2be1b5947ad6fc3c54511ecb70
ptobechi <ptobechii@gmail.com>
2023-07-20 12:15:46 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,25 +7,27 @@
  */
 int main(void)
 {
-	int i = 1;
+int i = 1;
 
-	for (; i <= 100; i++)
-	{
-		if (i%3 == 0 && i%5 == 0 )
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i%5 == 0) 
-		{
-			printf("Buzz ");
-		}
-		else if (i%3 == 0)
-		{
-			printf("Fizz ");
-		}else{
-			printf("%d ", i);
-		}
-	}
-	printf("\n");
-	return (0);
+for (; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 5 == 0) 
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
 }
