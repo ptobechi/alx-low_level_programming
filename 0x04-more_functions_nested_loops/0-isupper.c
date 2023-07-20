e386794685b35bd74311848ddc8f8117f57a6ebc
ptobechi <ptobechii@gmail.com>
2023-07-20 02:11:38 -0700

    check if characteris uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+i#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - returns 1 if true else return 0
+ * @c: check if uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    if (_isupper(c))
+    {
+	    return (1);
+    }
+    else
+    {
+	    return (0);
+    }
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Author: Tobechukwu Paschal
+ * Description: A declaration for all the function used i
+ *  0x04-more_functions_nested_loops project
+ */
+
+int _isupper(int c);
