cbb1686ace9cfc6abf6e64275d33623880fa0928
ptobechi <ptobechii@gmail.com>
2023-07-20 12:17:25 -0700

    fuzzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
- */
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
 int main(void)
 {
-int i = 1;
+int num;
+while (num++ < 100)
 
-for (; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-{
+if ((num % 3 == 0) && (num % 5 == 0))
 printf("FizzBuzz ");
-}
-else if (i % 5 == 0) 
-{
-printf("Buzz ");
-}
-else if (i % 3 == 0)
-{
+
+else if ((num % 3) == 0)
 printf("Fizz ");
-}
-else
+
+else if ((num % 5) == 0)
 {
-printf("%d ", i);
+if (num != 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
 }
-}
+
+else
+printf("%d ", num);
+
 printf("\n");
 return (0);
 }
