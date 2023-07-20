8d3725c26b3833ae3d55a00cdc547fccc2c899e4
ptobechi <ptobechii@gmail.com>
2023-07-20 12:00:35 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * print_line - draws a straight line
- * @n: int n distance for straing line
- */
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
 void print_line(int n)
 {
-	int count = 0;
-
-	if (n == 0)
-	{
-		_putchar('\n');
-	}else{
-		do{
-			_putchar('-');
-		}while(count < n)
-	}
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
