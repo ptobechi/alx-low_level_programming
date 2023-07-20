44b8fcae049bda612144e773b1e10f18e09c92eb
ptobechi <ptobechii@gmail.com>
2023-07-20 05:08:09 -0700

    complete project for more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,8 @@
 /**
  * _isupper - returns 1 if true else return 0
  * @c: check if uppercase.
- *
  * Return: Always 0 or 1
- */
+*/
 int _isupper(int c)
 {
     if (c >= 65 && c <= 90)
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,21 +1,16 @@
 #include "main.h"
-#include <ctype.h>
+#include <stdio.h>
 
 /**
  * _isdigit - returns 1 if true else returns 0
  * @c: char c to check
  * Return: Always 0.
  */
-int _isdigit(void)
+int _isdigit(int c)
 {
-    char c;
-
-    if (isdigit(c))
-    {
-	    return (1);
-    }
-    else
-    {
-	    return (0);
-    }
+	if(c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
 }
