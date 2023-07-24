0f8ed1545bfd5a19d1be9b0e5ea155cb5ce51869
ptobechi <ptobechii@gmail.com>
2023-07-24 13:41:16 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,9 +11,9 @@ void rev_string(char *s)
 {
 	int len = strlen(s);
 	int start = 0;
-	int end = len -1;
+	int end = len - 1;
 
-	while(start < end)
+	while (start < end)
 	{
 		char temp = s[start];
 		s[start] = s[end];
