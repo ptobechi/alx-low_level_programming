4a55c45bb51555caffb0717eae77054e23f0aba8
ptobechi <ptobechii@gmail.com>
2023-07-27 05:25:37 -0700

    capitalize words in a sentence

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,21 +6,32 @@
 char *cap_string(char *str)
 {
 	int i, j, found;
-	int word_seperators = {32, 9, 44, 10, 59, 46, 33, 63,34, 40, 41, 123, 125};
+	int word_seperators[] = {32, 9, 44, 10, 59, 46, 33, 63,34, 40, 41, 123, 125};
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+
+	if (str[i] >= 97 && str[i] <= 122)
+		str[i] = str[i] - 32;
+	
+	i++;
+
+	for (; str[i] != '\0'; i++)
 	{
+		found = 0;
+
 		for (j = 0; j < 13; j++)
 		{
-			if (str[i] == word_seperators)
+			if (str[i] == word_seperators[j])
 			{
 				found = 1;
 				break;
 			}
 		}
-		if (found = 1 && (str[i] >= 97 && str[i] <= 122))
+		if ((found == 1) && (str[i + 1] >= 97 && str[i + 1] <= 122))
+		{
+			i++;
 			str[i] = str[i] - 32;
-		i++
-		found = 0;
+		}
 	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -5,13 +5,13 @@
  * main - check the code
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-    char str[] = "Look up!\n";
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
     char *ptr;
 
-    ptr = string_toupper(str);
+    ptr = cap_string(str);
     printf("%s", ptr);
     printf("%s", str);
     return (0);
