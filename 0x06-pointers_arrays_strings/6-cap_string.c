5ed8851d9a8e6e1f3129905c5017971cd18fa5f5
ptobechi <ptobechii@gmail.com>
2023-07-27 05:50:28 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,13 +6,13 @@
 char *cap_string(char *str)
 {
 	int i, j, found;
-	int word_seperators[] = {32, 9, 44, 10, 59, 46, 33, 63,34, 40, 41, 123, 125};
+	int word_seperators[] = {32, 9, 44, 10, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	i = 0;
 
 	if (str[i] >= 97 && str[i] <= 122)
 		str[i] = str[i] - 32;
-	
+
 	i++;
 
 	for (; str[i] != '\0'; i++)
