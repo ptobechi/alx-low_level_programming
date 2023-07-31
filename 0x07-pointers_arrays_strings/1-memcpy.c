8f83efc667171999047088431a202e342a3c341f
ptobechi <ptobechii@gmail.com>
2023-07-31 08:51:11 -0700

    task one

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,6 +3,7 @@
  * @dest: destination for copied memory area
  * @src: string memory area to copy from
  * @n: n bytes to be copied
+ * Return: Always 0 (Success)
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
