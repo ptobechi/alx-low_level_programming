e54d697aeaf8d6a42377d79531fd18d46a37454a
ptobechi <ptobechii@gmail.com>
2023-08-02 07:46:05 -0700

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@
  * file: main.h
  * description: Prototype used for the 0x08-recursive projct
 */
-int _putchar(char);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
