bf4e4eb62ffeceead7904ecc7351801d00fb91e0
ptobechi <ptobechii@gmail.com>
2023-08-02 07:34:04 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ * Result: Always 0 (Succes)
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+	}
+	else
+	{
+		printf(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+/**
+ * Author: Mamah Tobechukwu Paschal
+ * file: main.h
+ * description: Prototype used for the 0x08-recursive projct
+*/
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+#endif
