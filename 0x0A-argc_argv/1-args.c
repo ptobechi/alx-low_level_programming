c1fbcd922ae6822b95a60f1583be3254681bd082
ptobechi <ptobechii@gmail.com>
2023-08-04 05:03:58 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: Argument array
  * Return: Always 0 (Success)
 */
-int main(int argc, char* argv[])
+int main(int argc, char* argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
