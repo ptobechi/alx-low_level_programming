1b34f1c4c2f906b7b69752cd62fbba7513217036
ptobechi <ptobechii@gmail.com>
2023-08-04 04:54:47 -0700

    uoda

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: Argument count
+ * @argv: Argument array
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char* argv[])
+{
+	if (argc < 3)
+		printf("Error\n");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (1);
+}
