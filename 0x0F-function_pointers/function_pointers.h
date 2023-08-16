4a310be3db84c13a5518093f969c0ba845ce6601
ptobechi <ptobechii@gmail.com>
2023-08-16 07:22:38 -0700

    prinnt name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,6 +5,9 @@
  * Author: Tobechukwu Paschal
  * file: function_pointers.h
  * description: Function prototypes for the funnction pointer proect
+ * print_name - print name
+ * @name: string name
+ * @f: function pointer
 */
 void print_name(char *name, void (*f)(char *));
 #endif
