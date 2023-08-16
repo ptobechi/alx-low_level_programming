f5326f21f591c0b9ec1523b65ec46c247bcd9756
ptobechi <ptobechii@gmail.com>
2023-08-16 07:42:29 -0700

    array of functions iterationn

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - Executes functions in an array
+ * @size: sizeof the array
+ * @array: array of element
+ * Return: Always 0 (Success)
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array && size && action)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -12,4 +12,5 @@
  * @f: function pointer
 */
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
