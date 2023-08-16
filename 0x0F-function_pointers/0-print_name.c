6ee48669fd0f6f679c4e429c53ffd81c8ce2702a
ptobechi <ptobechii@gmail.com>
2023-08-16 07:17:13 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,11 @@
 /**
  * print_name - prinnts a name
  * @name: string name
+ * @f: a function pointer
  * Return: Always 0 (success)
 */
 void print_name(char *name, void (*f)(char *))
 {
-	char *str = f(name);
-
-	printf("%s", str);
+	if (name && f)
+		f(name);
 }
