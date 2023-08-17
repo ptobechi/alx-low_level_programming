e1c895ed2ca4b40140e7e446683719674ee89142
ptobechi <ptobechii@gmail.com>
2023-08-17 03:09:58 -0700

    calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,3 +8,21 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+        	{"+", op_add},
+        	{"-", op_sub},
+        	{"*", op_mul},
+        	{"/", op_div},
+        	{"%", op_mod},
+        	{NULL, NULL}
+	};
+	int i;
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+
+	return (ops[i / 2].f);
+}
