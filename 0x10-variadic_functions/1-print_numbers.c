f1b9a956cd8bc9d280105de34ec43521dc471794
ptobechi <ptobechii@gmail.com>
2023-08-17 04:17:56 -0700

    variadic functionns

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numbers followed by a new line
+ * @separator: seperators for each numbers
+ * @n: list number to print
+ * Return: Always 0 (Success)
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	var_list list;
+
+	if (seperator == NULL)
+		return (0);
+	var_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s ", var_arg(list, int), seperator);
+	}
+	printf("\n");
+}
