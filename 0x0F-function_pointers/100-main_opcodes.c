26e1e7fbcc857539a0587b45a956655b6ccb7baf
ptobechi <ptobechii@gmail.com>
2023-08-18 09:56:20 -0700

    100 opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -35,13 +35,13 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
 	if (nb <= 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
 	print_opcodes((void *)main, nb);
