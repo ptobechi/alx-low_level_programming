9f998440dfc1b3f4719ca31f5f5eb8c06a9e7aa0
ptobechi <ptobechii@gmail.com>
2023-08-18 09:48:12 -0700

    header correction

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,14 +3,6 @@
 
 #include <stdlib.h>
 
-/**
- * Author: Tobechukwu Paschal
- * file: function_pointers.h
- * description: Function prototypes for the funnction pointer proect
- * print_name - print name
- * @name: string name
- * @f: function pointer
-*/
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
