ec24f1e86b0b06e1f71af23e4af913b4688a8956
ptobechi <ptobechii@gmail.com>
2023-08-28 14:17:50 -0700

    count number of element in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - return the number of element in a list
+ * @h: list head
+ *
+ * Return: Always 0 (Success)
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0; /* initialize count for number of element*/
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count)
+}
+
