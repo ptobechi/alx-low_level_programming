289e988bef739656a1985d6ccfe80cc81fa79e9c
ptobechi <ptobechii@gmail.com>
2023-08-31 15:30:50 -0700

    convert unnsigned int to binary

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ *Author: Mamah Tobechukwu Paschal
+ *file: main_h
+ *description: bit manipulation prototypes
+*/
+unsigned int binary_to_uint(const char *b);
+#endif /*_MAIN_H_*/
