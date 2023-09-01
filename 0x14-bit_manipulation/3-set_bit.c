6233b33a03804e92e967629b8e05ee9b5cdb2d10
ptobechi <ptobechii@gmail.com>
2023-08-31 21:47:40 -0700

    sets nnumber bit to1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1
+ * @n: number pointer
+ * index: indexof number
+ *
+ * Return: Always 0 (Success)
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n |= (1 << index);
+	return (1);
+}
