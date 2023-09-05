b6d05fe7121aa84976e0371d5d4db11235f5d2e8
Tobechukwu <ptobechii@gmail.com>
2023-09-05 15:13:13 +0000

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create-file - creates a file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: Always 0 (Success)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	FILE *file_ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_ptr = fopen(filename, "w");
+
+	if (file_ptr == NULL)
+		return (-1);
+
+	while(*text_content != '\0')
+	{
+		fputc(*text_content, file_ptr);
+		text_content++;
+	}
+	return (1);
+}
