0126ca5264df58dce1f5b6b699890a8f3072f663
Tobechukwu <ptobechii@gmail.com>
2023-09-23 03:27:30 -0500

    betty issues fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,12 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Author: Mamah Tobechukwu Paschal
- * file: main_h
- * Description: bit manipulation prototypes
- */
-
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
