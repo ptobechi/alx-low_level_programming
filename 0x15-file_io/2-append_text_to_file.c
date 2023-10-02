13c43a99b831eb18673cca5464998a8948befe8d
Tobechukwu <ptobechii@gmail.com>
2023-10-02 07:56:21 -0500

    add content to the end of a file (append) function

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - add text content to the end of a file
+ * @filename: name of the file to add content to
+ * @text_content: string of null terminated char to add to the end of the file
+ *
+ * Return: Always 0 (Success)
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *file_ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	/*open file in append mode*/
+	file_ptr = fopen(filename, "a");
+
+	/* check edge cases for text content and file avail */
+	if (text_content == NULL && file_ptr != NULL)
+		return (1);
+
+	if (text_content == NULL && file_ptr == NULL)
+		return (-1);
+
+	/*append to file */
+	while (*text_content != '\0')
+	{
+		fputc(*text_content, file_ptr);
+		text_content++;
+	}
+	fclose(file_ptr);/*close file*/
+
+	return (1);
+}
