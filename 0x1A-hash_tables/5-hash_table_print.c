7c470dfe59b64bd7996180ebce80165d829d4011
ptobechi <ptobechii@gmail.com>
2023-12-06 08:37:38 -0600

    corrections

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,6 +7,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
+	int printed = 0;
 
 	if (ht == NULL)
 		return;
@@ -19,7 +20,8 @@ void hash_table_print(const hash_table_t *ht)
 		while (current != NULL)
 		{
 			printf("'%s': '%s'", current->key, current->value);
-			if (i < ht->size - 1 || current->next != NULL)
+			if ((i < ht->size - 1 || current->next != NULL)
+				&& printed)
 				printf(", ");
 			current = current->next;
 		}
